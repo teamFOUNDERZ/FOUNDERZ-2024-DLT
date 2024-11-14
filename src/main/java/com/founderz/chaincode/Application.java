fa6b0f430a3ec72310850ac8097c9854b98f448b
101 <tmddnjs001133@dsm.hs.kr>
2024-11-14 09:27:25 +0900

    update: change to hyperledger.fabric

--- src/main/java/com/founderz/chaincode/Application.java
+++ src/main/java/com/founderz/chaincode/Application.java
@@ -1,13 +1,7 @@
 package com.founderz.chaincode;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class FounderzChainCodeApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FounderzChainCodeApplication.class, args);
 	}
-
 }
