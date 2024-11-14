6937163000937c58dcacf49e26959caa042d1863
101 <tmddnjs001133@dsm.hs.kr>
2024-11-14 09:24:51 +0900

    init: project

---
+++ src/main/java/com/founderz/chaincode/Application.java
@@ -0,0 +1,13 @@
+package com.founderz.chaincode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FounderzChainCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FounderzChainCodeApplication.class, args);
+	}
+
+}
